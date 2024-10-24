7da0a1dd5ea0642af6db89b0f97f8d4843987d6c
Andre Tanakaa <tanaka.s.andre@gmail.com>
2024-10-24 19:28:46 +1000

    andrea

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <strong>HELLOOWW ANDREA</strong>
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
